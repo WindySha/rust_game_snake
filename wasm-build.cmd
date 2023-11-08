e23b89bade1e60a546ff4b8cf7ffdfdf8c39748a
xiawanli <xiawanli@bytedance.com>
2023-11-09 03:33:55 +0800

    add github action to support publish web page

--- wasm-build.cmd
+++ wasm-build.cmd
@@ -1,4 +1,6 @@
 
  cargo build --target wasm32-unknown-unknown --release --features web
  wasm-bindgen --out-dir ./out/ --target web ./target/wasm32-unknown-unknown/release/snake.wasm
- python3 -m http.server
+ cp index.html out
+ cp -R assets out
+
